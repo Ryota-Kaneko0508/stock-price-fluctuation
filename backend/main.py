946d061c16fb32522e105f5a449392b3062fdd4e
Ryota-Kaneko0508 <kane2508ryo@gmail.com>
2026-01-22 16:44:28 +0900

    ユーザーが購読している株価の銘柄の一覧データ（後述）を返す」APIを実装

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,18 @@ from fastapi import Depends, FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
+import yfinance as yf
+import pandas as pd
+
 class Users(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    email: str
+    ID: int | None = Field(default=None, primary_key=True)
+    Email: str
+
+class Notifications(SQLModel, table=True):
+    ID: int | None = Field(default=None, primary_key=True)
+    UserID: int = Field(foreign_key="users.ID")
+    Tick: str
+    Status: bool
 
 database_url = "postgresql+psycopg2://postgres:password@db:5432/postgresDB"
 
@@ -36,10 +45,49 @@ class UserRequest(BaseModel):
     id: str
     email: str
 
+class Stock(BaseModel):
+    tick: str
+    company: str
+    price_today: float
+    price_yesterday: float
+
 @app.post("/users")
 async def create_user(user_request: UserRequest, session: SessionDep) -> Users:
-    new_user = Users(email = user_request.email)
+    new_user = Users(Email = user_request.email)
     session.add(new_user)
     session.commit()
     session.refresh(new_user)
     return new_user
+
+@app.get("/stocks/")
+async def get_stocks(user_id: int, session: SessionDep) -> list[Stock]:
+    notifications = session.exec(select(Notifications).where(Notifications.UserID == user_id)).all()
+
+    res = []
+
+    for notification in notifications:
+
+        tick = notification.Tick
+
+        #Tikerで一つの銘柄の情報を取得
+        STOCK = yf.Ticker(tick) 
+
+        # 情報取得(.info)
+        STOCK_info = STOCK.info
+
+        company_name = STOCK_info["longName"]
+        price_today = STOCK_info["currentPrice"]
+        price_yesterday = STOCK_info["regularMarketPreviousClose"]
+
+        stock_data = Stock(
+            tick=tick,
+            company=company_name,
+            price_today=price_today,
+            price_yesterday=price_yesterday
+        )
+
+        res.append(stock_data)
+
+    return res
+
+
